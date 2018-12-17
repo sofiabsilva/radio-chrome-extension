44a16c03bff1b2228e9c15ffd3377d405516ef02
sofiabsilva <sofi.bsilva@gmail.com>
2018-12-17 20:03:37 +0000

    initial options js

---
+++ options.js
@@ -0,0 +1,30 @@
+// from https://developer.chrome.com/extensions/options
+// Saves options to chrome.storage
+function save_options() {
+  var selected = document.querySelector('.ios-switch').checked;
+
+  chrome.storage.sync.set({
+    mute: selected
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.querySelector('#status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default checked selected = true.
+  chrome.storage.sync.get({
+    mute: true
+  }, function(items) {
+    document.querySelector('.ios-switch').checked = items.mute;
+  });
+}
+// document.addEventListener('DOMContentLoaded', restore_options);
+// document.getElementById('save').addEventListener('click',
+//     save_options);
