e4ef354bfe5b41eb19eb43fed8d9482846ce2db7
sofiabsilva <sofi.bsilva@gmail.com>
2018-12-18 13:17:01 +0000

    make reusable functions for play and stop

--- background.js
+++ background.js
@@ -1,15 +1,22 @@
 let audio = new Audio();
 
+function playAudio() {
+  audio.src = "http://europa.shoutca.st:8648/;/;stream.mp3";
+  audio.play();
+}
+function stopAudio() {
+  audio.pause();
+  audio.src = "";
+  audio = new Audio();
+  console.log(audio.src);
+}
+
 // Receiving messages from popup.js to play and stop audio
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.status == "playing") {
-        audio.src = "http://europa.shoutca.st:8648/;/;stream.mp3";
-        audio.play();
+        playAudio();
     } else if (message.status == "stopped") {
-      audio.pause();
-      audio.src = "";
-      audio = new Audio();
-      console.log(audio.src);
+        stopAudio();
     } else if (message.status == "loaded") {
       // Every time page loads check if audio is playing if so, send response "toggleIcon"
         if (!audio.paused) {
