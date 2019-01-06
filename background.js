bcf7c4d481af49530ca3bf6903848aa0307bb677
Rahul Gurung <gurungrahul2@gmail.com>
2019-01-06 21:01:44 +0530

    #2 - added Travis CI lint check
    when I did lint run it gave
    many errors, I resolved them
    and pushed here, I have not
    changed the logic but only
    styling.

--- background.js
+++ background.js
@@ -1,54 +1,54 @@
-let audio = new Audio();
+let audio = new Audio()
 
 // Store default option
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({
     mute: true
   })
 })
 
 // Option to turn off sound of extension when another tab is audible
-let tabAudio;
-function checkTabs() {
-  chrome.storage.sync.get(['mute'], function(result) {
-          if (result.mute) {
-            chrome.tabs.query({audible: true}, function callback(data) {
-              // When a tab is audible
-              if (!audio.paused && data.length > 0) {
-                  tabAudio = true;
-                  stopAudio();
-              } else if (tabAudio && data.length == 0) {
-                  tabAudio = false;
-                  playAudio();
-              }
-            });
-          }
-        });
+let tabAudio
+function checkTabs () {
+  chrome.storage.sync.get(['mute'], function (result) {
+    if (result.mute) {
+      chrome.tabs.query({ audible: true }, function callback (data) {
+        // When a tab is audible
+        if (!audio.paused && data.length > 0) {
+          tabAudio = true
+          stopAudio()
+        } else if (tabAudio && data.length === 0) {
+          tabAudio = false
+          playAudio()
+        }
+      })
+    }
+  })
 }
 
-setInterval(checkTabs, 1000);
+setInterval(checkTabs, 1000)
 
-function playAudio() {
-  audio.src = "http://europa.shoutca.st:8648/;/;stream.mp3";
-  audio.play();
+function playAudio () {
+  audio.src = 'http://europa.shoutca.st:8648/;/;stream.mp3'
+  audio.play()
 }
-function stopAudio() {
-  audio.pause();
-  audio.src = "";
-  audio = new Audio();
+function stopAudio () {
+  audio.pause()
+  audio.src = ''
+  audio = new Audio()
 }
 
 // Receiving messages from popup.js to play and stop audio
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.status == "playing") {
-        playAudio();
-        checkTabs();
-    } else if (message.status == "stopped") {
-        stopAudio();
-    } else if (message.status == "loaded") {
-      // Every time page loads check if audio is playing if so, send response "toggleIcon"
-        if (!audio.paused) {
-            sendResponse({action: "toggleIcon"});
-        }
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.status === 'playing') {
+    playAudio()
+    checkTabs()
+  } else if (message.status === 'stopped') {
+    stopAudio()
+  } else if (message.status === 'loaded') {
+    // Every time page loads check if audio is playing if so, send response 'toggleIcon'
+    if (!audio.paused) {
+      sendResponse({ action: 'toggleIcon' })
     }
-});
+  }
+})
--- popup.js
+++ popup.js
@@ -1,122 +1,122 @@
-const player = document.querySelector(".player");
-const drawer = document.querySelector("#drawer");
-const drawerIcon = document.querySelector(".drawerIcon");
-player.addEventListener("click", toggleAudio);
-drawer.addEventListener("click", lastAired);
+const player = document.querySelector('.player')
+const drawer = document.querySelector('#drawer')
+const drawerIcon = document.querySelector('.drawerIcon')
+player.addEventListener('click', toggleAudio)
+drawer.addEventListener('click', lastAired)
 
 // When not being clicked:
-drawerIcon.removeAttribute("style");
+drawerIcon.removeAttribute('style')
 // Getting current songs from centovacast JSON
-function currentSongs() {
-  const songInfo = document.querySelector("#currentSong");
-  const albumCover = document.querySelector("#cover");
-  let content;
-  $.getJSON('https://europa.shoutca.st/rpc/atsueste/streaminfo.get', function(data) {
-      content = `<ul>
-      <li class="artist">${data.data[0]['track']['artist']}</li>
-      <li class="songTitle">${data.data[0]['track']['title']}</li>
-      </ul>`;
-      albumCover.src=`${data.data[0]['track']['imageurl']}`;
-      songInfo.innerHTML= content;
-  });
+function currentSongs () {
+  const songInfo = document.querySelector('#currentSong')
+  const albumCover = document.querySelector('#cover')
+  let content
+  $.getJSON('https://europa.shoutca.st/rpc/atsueste/streaminfo.get', function (data) {
+    content = `<ul>
+    <li class="artist">${data.data[0]['track']['artist']}</li>
+    <li class="songTitle">${data.data[0]['track']['title']}</li>
+    </ul>`
+    albumCover.src = `${data.data[0]['track']['imageurl']}`
+    songInfo.innerHTML = content
+  })
 }
 
 // Update song info every 4 seconds
-setInterval(currentSongs, 4000);
+setInterval(currentSongs, 4000)
 
 // Flag to keep track if drawer is open
-let open = false;
+let open = false
 // Get the 10 last aired songs
-function lastAired() {
+function lastAired () {
   // Toggle icons
-  drawerIcon.classList.toggle("fa-angle-down");
-  drawerIcon.classList.toggle("fa-angle-up");
-  const songsList = document.querySelector("#recentSongs");
+  drawerIcon.classList.toggle('fa-angle-down')
+  drawerIcon.classList.toggle('fa-angle-up')
+  const songsList = document.querySelector('#recentSongs')
 
-  if (drawerIcon.classList.contains("fa-angle-down")) {
-      drawerIcon.classList.add("open");
-      drawerIcon.classList.remove("closed");
+  if (drawerIcon.classList.contains('fa-angle-down')) {
+    drawerIcon.classList.add('open')
+    drawerIcon.classList.remove('closed')
   }
   // If drawerIcon contains angle up when button clicked
-  if (drawerIcon.classList.contains("fa-angle-up")) {
-      drawerIcon.classList.remove("open");
-      drawerIcon.classList.add("closed");
+  if (drawerIcon.classList.contains('fa-angle-up')) {
+    drawerIcon.classList.remove('open')
+    drawerIcon.classList.add('closed')
   }
   if (!open) {
-    let content;
+    let content
     // Loading animation
-    if (typeof(content) == "undefined") {
-    // http://www.ajaxload.info/
-    recentSongs.innerHTML= "<img class='loading' alt='loading' src='images/ajax-loader.gif'/>";
+    if (typeof (content) === 'undefined') {
+      // http://www.ajaxload.info/
+      recentSongs.innerHTML = "<img class='loading' alt='loading' src='images/ajax-loader.gif'/>"
     }
-    $.getJSON('https://europa.shoutca.st/recentfeed/atsueste/json/', function(data) {
-      content = `<ul id ="airedList">`
-      let lastFive = Object.entries(data.items).slice(1,5);
-        for (let i = 0; i < lastFive.length; i++) {
-          content += `<li>
-          <img class="albumCover" src="${lastFive[i][1]['enclosure']['url']}" alt="${lastFive[i][1]['description']}">
-          ${lastFive[i][1]['title']}</li>`;
-        }
-      content +=`</ul>`;
-      recentSongs.innerHTML= content;
+    $.getJSON('https://europa.shoutca.st/recentfeed/atsueste/json/', function (data) {
+      content = `<ul id ='airedList'>`
+      let lastFive = Object.entries(data.items).slice(1, 5)
+      for (let i = 0; i < lastFive.length; i++) {
+        content += `<li>
+        <img class='albumCover' src="${lastFive[i][1]['enclosure']['url']}" alt="${lastFive[i][1]['description']}">
+        ${lastFive[i][1]['title']}</li>`
+      }
+      content += `</ul>`
+      recentSongs.innerHTML = content
     })
-    open = true;
+    open = true
   } else {
-    open = false;
-    airedList.classList.toggle("slideUp");
-    setTimeout(function(){songsList.removeChild(songsList.firstChild);}, 300);
+    open = false
+    airedList.classList.toggle('slideUp')
+    setTimeout(function () { songsList.removeChild(songsList.firstChild) }, 300)
   }
 }
 
 // When popup is open get current songs and when the audio is already playing change the player icon
-window.onload = function() {
-  currentSongs();
-  chrome.runtime.sendMessage({status: "loaded"}, function(response) {
-    if (response.action == "toggleIcon") {
-      player.classList.toggle("fa-stop-circle");
-      player.classList.toggle("fa-play-circle");
+window.onload = function () {
+  currentSongs()
+  chrome.runtime.sendMessage({ status: 'loaded' }, function (response) {
+    if (response.action === 'toggleIcon') {
+      player.classList.toggle('fa-stop-circle')
+      player.classList.toggle('fa-play-circle')
     }
-  });
+  })
 }
 
 // Change the player icon and send message to background script to play stream (even when the popup is closed)
-function toggleAudio() {
-  if (player.classList.contains("fa-play-circle")) {
-    chrome.runtime.sendMessage({status: "playing"}, function() {
-      player.classList.toggle("fa-stop-circle");
-      player.classList.toggle("fa-play-circle");
-    });
-  } else if (player.classList.contains("fa-stop-circle")) {
-      chrome.runtime.sendMessage({status: "stopped"}, function() {
-        player.classList.toggle("fa-stop-circle");
-        player.classList.toggle("fa-play-circle");
-      });
+function toggleAudio () {
+  if (player.classList.contains('fa-play-circle')) {
+    chrome.runtime.sendMessage({ status: 'playing' }, function () {
+      player.classList.toggle('fa-stop-circle')
+      player.classList.toggle('fa-play-circle')
+    })
+  } else if (player.classList.contains('fa-stop-circle')) {
+    chrome.runtime.sendMessage({ status: 'stopped' }, function () {
+      player.classList.toggle('fa-stop-circle')
+      player.classList.toggle('fa-play-circle')
+    })
   };
 }
 
 // Animate drawerIcon on whole drawer div
-drawer.addEventListener("mouseover", function transitionOn() {
-  drawerIcon.style.color = "#e84724";
+drawer.addEventListener('mouseover', function transitionOn () {
+  drawerIcon.style.color = '#e84724'
 })
-drawer.addEventListener("mouseout", function transitionOff() {
-  drawerIcon.style.color = "#41ab3d";
+drawer.addEventListener('mouseout', function transitionOff () {
+  drawerIcon.style.color = '#41ab3d'
 })
 
 // Detecting the navigator language and changing title
-let userLang = navigator.language || navigator.userLanguage;
-let title = document.querySelector('.title');
-let pt = /^pt/i;
-let fr = /^fr/i;
-let es = /^es/i;
-let de = /^de/i;
+let userLang = navigator.language || navigator.userLanguage
+let title = document.querySelector('.title')
+let pt = /^pt/i
+let fr = /^fr/i
+let es = /^es/i
+let de = /^de/i
 if (pt.test(userLang)) {
-  title.innerText = "música sem fronteiras";
+  title.innerText = 'música sem fronteiras'
 } else if (fr.test(userLang)) {
-  title.innerText = "musique sans frontières";
+  title.innerText = 'musique sans frontières'
 } else if (es.test(userLang)) {
-  title.innerText = "música sin fronteras";
-} else if (de.test(userLang)){
-  title.innerText = "musik ohne grenzen";
+  title.innerText = 'música sin fronteras'
+} else if (de.test(userLang)) {
+  title.innerText = 'musik ohne grenzen'
 } else {
-  title.innerText = "music without frontiers";
+  title.innerText = 'music without frontiers'
 };
